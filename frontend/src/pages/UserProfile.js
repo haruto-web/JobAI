364deea6998167dfc89bf569e0844bd0fa361d18
haruto-web <venandrewmirasol@gmail.com>
2025-11-12 13:59:38 +0800

    last update

--- frontend/src/pages/UserProfile.js
+++ frontend/src/pages/UserProfile.js
@@ -118,9 +118,6 @@ function UserProfile() {
                   <div className="skills-container">
                     {user.profile.skills.map((skill, index) => (
                       <span key={index} className="skill-tag">
-                        <svg width="14" height="14" viewBox="0 0 20 20" fill="currentColor">
-                          <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                        </svg>
                         {skill}
                       </span>
                     ))}
